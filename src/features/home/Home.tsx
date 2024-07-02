8394d461137f0a60d4486cea3c077812187beb75
Sohail Shrestha <sohailshrestha2@gmail.com>
2024-07-03 00:07:43 +0545

    added grid for left and right content

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,12 +1,17 @@
+import { Grid } from "@mui/material";
 import React from "react";
 import { HomeLeftComponent, HomeRightComponent } from "./components";
 
 const Home: React.FC = () => {
   return (
-    <>
-      <HomeLeftComponent />
-      <HomeRightComponent />
-    </>
+    <Grid container>
+      <Grid item xs={6}>
+            <HomeLeftComponent />
+      </Grid>
+      <Grid item xs={6}>
+            <HomeRightComponent />
+      </Grid>
+    </Grid>
   );
 };
 
